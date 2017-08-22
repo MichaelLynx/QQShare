c53472ae9be2a100a5a537289eb97305c46a4e99
BlackSage <1216831710@qq.com>
2017-08-22 10:18:33 +0800

    Initial Commit

---
+++ QQShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQShare
+//
+//  Created by Admin on 2017/8/22.
+//  Copyright © 2017年 DB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQShare
+//
+//  Created by Admin on 2017/8/22.
+//  Copyright © 2017年 DB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
